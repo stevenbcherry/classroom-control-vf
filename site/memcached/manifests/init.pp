7f57bffca33b4688311b694a267efc02c9401c08
Steven B Cherry <scherry@nabancard.com>
2016-04-05 15:53:29 -0400

    Create init.pp
    introducing the memcached class

---
+++ site/memcached/manifests/init.pp
@@ -0,0 +1,13 @@
+
+class memcached{
+
+ #TODO
+ 
+ # manage
+ # The memcached package.
+ # The /etc/sysconfig/memcached configuration file using the
+ # example settings provided above.
+ # The memcached service
+
+}
+
