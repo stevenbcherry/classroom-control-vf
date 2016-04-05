147ba40e5feadd2151e16703af98f6f7771e5952
Steven B Cherry <scherry@nabancard.com>
2016-04-05 13:26:43 -0400

    Update init.pp

--- site/users/manifests/init.pp
+++ site/users/manifests/init.pp
@@ -1,15 +1,7 @@
 
 # ensure fundementals user exists
 
-user { 'fundementals':
-  ensure           => 'present',
-  # gid              => '10',
-  home             => '/home/fundementals',
-  # password         => '!!',
-  password_max_age => '99999',
-  password_min_age => '0',
-  shell            => '/bin/bash',
-  # uid              => '1001',
+user { 'fundamentals':
+  ensure => present,
 }
-
 
