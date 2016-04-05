9ac7cf9df8c978c6a927b109f6f595cb203f970f
Steven B Cherry <scherry@nabancard.com>
2016-04-05 15:12:27 -0400

    Update init.pp
    modules/skeleton/.bashrc

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -21,7 +21,7 @@ class skeleton {
     group   => 'root',
     mode    => '0644',
     # content => "# test bashrc file ! Ooo. ",
-    source  =>  'puppet:///skeleton/.bashrc',
+    source  =>  'puppet:///modules/skeleton/.bashrc',
   }
   
   
