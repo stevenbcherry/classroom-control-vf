a12ce05ccade34c2b855df5e8d5fa899ea2932e0
Steven B Cherry <scherry@nabancard.com>
2016-04-05 13:22:55 -0400

    Update init.pp
    start of my own users module, ensuring user fundementals is present.

--- site/users/manifests/init.pp
+++ site/users/manifests/init.pp
@@ -1,2 +1,15 @@
 
 # ensure fundementals user exists
+
+user { 'fundementals':
+  ensure           => 'present',
+  # gid              => '10',
+  home             => '/home/fundementals',
+  # password         => '!!',
+  password_max_age => '99999',
+  password_min_age => '0',
+  shell            => '/bin/bash',
+  # uid              => '1001',
+}
+
+
