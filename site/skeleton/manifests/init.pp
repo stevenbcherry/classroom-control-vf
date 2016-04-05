95b875c626bdb98534d3e1d2630c8fb15a64e1d5
Steven B Cherry <scherry@nabancard.com>
2016-04-05 14:07:37 -0400

    Create init.pp
    added skel and bashrc

---
+++ site/skeleton/manifests/init.pp
@@ -0,0 +1,31 @@
+class skeleton {
+
+  # add resources: 
+  
+  # directory /etc/skel
+ 
+  file{ 'skel':
+    ensure => directory,
+    path  => '/etc/skel',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',    
+  }
+  
+  # file /etc/skel/.bashrc
+  
+  file { 'bashrc':
+    ensure  => file,
+    path    => '/etc/skel/.bashrc',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => "# test bashrc file ! Ooo. ",
+  }
+  
+  # TODO
+  # should be sourced from a file in module users
+  
+  
+
+}
