f7ef017399fd1a7ba7205a9be73c5834b98c1c1b
Steven B Cherry <scherry@nabancard.com>
2016-04-06 10:19:05 -0400

    Update init.pp
    adding nginx package

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,1 +1,12 @@
-# Refactor common parameters into resource defaults.
+class nginx{
+
+  package { 'nginx':
+    ensure   => present,
+  }
+
+# /site/nginx/files/nginx.conf
+# /site/nginx/files/default.conf
+
+
+
+}
