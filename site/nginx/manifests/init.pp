79439d7e6dfb6887aeda7953834242abae04b134
Steven B Cherry <scherry@nabancard.com>
2016-04-06 10:43:35 -0400

    Update init.pp
    refactored with resource default file

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -4,21 +4,27 @@ class nginx{
     ensure   => present,
   }
 
+  file {
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',  
+  }
+
   file { 'nginx config':
     ensure  => file,
     path    => '/etc/nginx/nginx.conf',
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
+    # owner   => 'root',
+    # group   => 'root',
+    # mode    => '0644',
     source  =>  'puppet:///modules/nginx/nginx.conf',
   }
 
   file { 'nginx config default':
     ensure  => file,
     path    => '/etc/nginx/nginx.conf.default',
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
+    # owner   => 'root',
+    # group   => 'root',
+    # mode    => '0644',
     source  =>  'puppet:///modules/nginx/default.conf',
   }
 
