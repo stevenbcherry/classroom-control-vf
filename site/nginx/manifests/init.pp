2f81eea21d960496f5be2de690580b1a9d1dbfa5
Steven B Cherry <scherry@nabancard.com>
2016-04-06 10:27:39 -0400

    Update init.pp
    adding nginx

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -4,9 +4,29 @@ class nginx{
     ensure   => present,
   }
 
-# /site/nginx/files/nginx.conf
-# /site/nginx/files/default.conf
+  file { 'nginx config':
+    ensure  => file,
+    path    => '/etc/nginx/nginx.conf',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    source  =>  'puppet:///modules/nginx/nginx.conf',
+  }
+
+  file { 'nginx config default':
+    ensure  => file,
+    path    => '/etc/nginx/nginx.conf.default',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    source  =>  'puppet:///modules/nginx/default.conf',
+  }
 
 
+  service { 'nginx service':
+    name       => 'nginx',
+    ensure     => running,
+    enable     => true,
+  }
 
 }
